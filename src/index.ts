01224a015b2ecbe3cfba1c277da988140c5bf479
Samuele De Tomasi <el3um4s@gmail.com>
2022-08-17 15:44:21 +0200

    First version

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-const myCustomFunction = (name: string):string => `Hello ${name}`;
-function ciao(name: string): string {
-    return `Ciao ${name}`;
-}
+import windowControls from "./IPC/renderer";
 
-export { myCustomFunction, ciao};
+export default windowControls;
